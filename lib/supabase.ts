4dbec39bc2121ee898908de580ef45c678e39a12
Khoa Le <kle815@outlook.com>
2025-10-11 07:34:12 -0700

    add prettier

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,9 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://icbsxipnzmxxolkrtdmn.supabase.co";
+const supabaseUrl = 'https://icbsxipnzmxxolkrtdmn.supabase.co';
 const supabasePublishableKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljYnN4aXBuem14eG9sa3J0ZG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxMDk5NjAsImV4cCI6MjA3NTY4NTk2MH0.JCGNtbXmZYnDNwzwoTBT04xZBHCP4dJAQav5sDjbpOg";
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljYnN4aXBuem14eG9sa3J0ZG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxMDk5NjAsImV4cCI6MjA3NTY4NTk2MH0.JCGNtbXmZYnDNwzwoTBT04xZBHCP4dJAQav5sDjbpOg';
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
