9cf422663024a11138585cbb361051ec1871d87f
Khoa Le <kle815@outlook.com>
2025-11-12 07:48:43 -0800

    fix web

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,6 @@ import { Config } from '@/constants/environment-config';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-console.log(Config.SUPABASE_URL, Config.SUPABASE_PUBLISHABLE_KEY);
-
 export const supabase = createClient(
   Config.SUPABASE_URL,
   Config.SUPABASE_PUBLISHABLE_KEY,
